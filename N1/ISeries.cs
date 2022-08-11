63fcd2a0a79880d1da93a3b9ca112e8ec96d470b
Алексей Викулин <vaa@megamade.ru>
2022-08-11 11:21:24 +0300

    Добавьте файлы проекта.

---
+++ N1/ISeries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1
+{
+   interface ISeries    //в interface все public
+    {
+        void SetStart(int x);   //устанавливает начальное значение
+        int GetNext();  //возвращает следующее число ряда
+        void Reset();   //выполняет сброс к начальному значению
+
+    }
+}
---
+++ N1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgress arith = new ArithProgress();
+            arith.SetStart(0);
+            arith.SetStep(2);
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine(arith.GetNext());
+            arith.Reset();
+            Console.WriteLine(arith.GetNext());
+            Console.WriteLine(arith.GetNext());
+            GeomProgress geom = new GeomProgress();
+            geom.SetStart(2);
+            geom.SetStep(2);
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+            geom.Reset();
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+            Console.ReadKey();
+        }
+    }
+}
